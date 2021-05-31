90dc6121478ac4a5646d08add5755414c2ac0407
JanekZalewski <janekzalewski7@gmail.com>
2021-05-31 19:50:05 +0200

    2021-05-31 JZ

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-
+from re import split
 from app import app
 from app.models.product import Product
 from flask import render_template, redirect, url_for
@@ -14,13 +14,13 @@ def extract(productId):
     product = Product(productId)
     product.extractProduct()
     product.exportProduct()
-    return redirect(url_for("product", productId=productId))
+    return redirect(url_for('product', productId=productId))
     # return render_template('extract.html.jinja')
 
 @app.route('/products')
 def products():
     products = [product.split('.')[0] for product in listdir("app/products")]
-    return render_template('product.html.jinja', products=products)
+    return render_template('products.html.jinja', products=products)
 
 @app.route('/about')
 def about():
